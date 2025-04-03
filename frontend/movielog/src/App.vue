378a5e9f0b8c1f57b643a57fdf84c962f8ad9337
Eric <erikasmockus@gmail.com>
2025-04-03 21:58:31 +0300

    initial sidebar component

--- frontend/movielog/src/App.vue
+++ frontend/movielog/src/App.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView />
+  <div class="bg-dark-primary text-white h-screen w-screen flex px-6 py-2">
+    <SidebarMenu />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import SidebarMenu from './components/SidebarMenu.vue'
+</script>
 
 <style></style>
---
+++ frontend/movielog/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Welcome
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style></style>
